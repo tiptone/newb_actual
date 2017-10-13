ae1780ffd2de87cbc88c5ec4108e4e7af13b3f17
thedevilsvoice <franklin@bitsmasher.net>
2017-10-12 22:09:21 -0400

    added new follower msg

--- new_followers.py
+++ new_followers.py
@@ -10,22 +10,43 @@ import sched, time
 global api
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 s = sched.scheduler(time.time, time.sleep)
+last_count = api.get_user('253608265').followers_count
 
 def do_something(sc): 
+  global last_count
+  if (last_count > api.get_user('253608265').followers_count):
+    last_count = api.get_user('253608265').followers_count
+    print "Decrementing last_count"
   bat = api.get_user('253608265')
   print "Watching User: " , bat.screen_name
   #print bat.followers_count
-  s.enter(30, 1, do_something, (sc,))
+  print "Last count: " , last_count
+  s.enter(60, 1, do_something, (sc,))
   #api.update_status('test tweet') 
   for user in tweepy.Cursor(api.followers, screen_name="mzbat").items():
-    print "Most recent: " , user.screen_name 
-    print "Follower Count: " , bat.followers_count
-    if (bat.followers_count == 200000) :
-      api.update_status("@mzbat the 20K follower is " , user.screen_name )
+    if (bat.followers_count % 5 == 0 ) and (last_count != bat.followers_count):
+      print "Modulo 5"
+      print "Most recent: " , user.screen_name
+      print "Follower Count: " , bat.followers_count
+      api.update_status("@mzbat the follower count is ".format(bat.followers_count))
+      last_count = bat.followers_count
+    elif (bat.followers_count == 200000) :
+      print "20K"
+      print "Most recent: " , user.screen_name
+      print "Follower Count: " , bat.followers_count
+      api.update_status("@mzbat the 20K follower is ".format(mzbat.screen_name))
+      last_count = bat.followers_count
+    elif (bat.followers_count > last_count):
+      print "Gained a follower: " , user.screen_name
+      print "Follower Count: " , bat.followers_count
+    else: 
+      print "ho hum, nothing happened"
+      print "Most recent: " , user.screen_name
+      print "Follower Count: " , bat.followers_count
     break
   
-s.enter(30, 1, do_something, (s,))
+s.enter(5, 1, do_something, (s,))
 s.run()
