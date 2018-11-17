6b29e1e5daae2f89d7276ad80369d3ece104a95b
FrankTheTank <franklin@bitsmasher.net>
2018-11-17 21:24:32 +0000

    cleanup CI

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ Unit testing of the automatic batch processing application
 http://www.codingtricks.biz/ci-cd-python-project-with-gitlab/
 """
 import unittest
-from src.twitter_bot import unfollow_back_who_not_folow_me
+from twitter_bot.twitter_bot import unfollow_back_who_not_folow_me
 
 class AppTests(unittest.TestCase):
   def test_app(self):
