1944ee0131b0424e31d38d99e2a3e2ec4b9839d3
FrankTheTank <franklin@bitsmasher.net>
2018-11-21 02:41:46 +0000

    add a couple more commands

--- slack_bot/command.py
+++ slack_bot/command.py
@@ -1,4 +1,5 @@
 import datetime
+from weather import Weather, Unit
 
 class Command(object):
   def __init__(self):
@@ -6,6 +7,8 @@ class Command(object):
       "help" : self.help,
       "cookbook" : self.cookbook,
       "github" : self.github,
+      "music" : self.music,
+      "weather" : self.weather,
       "when" : self.when
     }
  
@@ -23,7 +26,19 @@ class Command(object):
     return "https://github.com/DEAD10C5/1337-Noms-The-Hacker-Cookbook"
   def github(self):
     return "https://github.com/theDevilsVoice/newb_actual/tree/master/slack_bot"
-
+  def music(self):
+    return "https://www.reverbnation.com/angusmohr/song/14449187-andy-renwicks-favourite-ferret"
+  def weather(self):
+    weather = Weather(unit=Unit.FAHRENHEIT)
+    lookup = weather.lookup(2400352)
+    condition = lookup.condition
+    #for forecast in forecasts:
+    #  print(forecast.text)
+    #  print(forecast.date)
+    #  print(forecast.high)
+    #  print(forecast.low)
+    response = "Currently: " + str(condition.temp) + " and " + str(condition.text) + " in beautiful Estes Park, CO"
+    return response
   def when(self):
     delta = datetime.datetime(2019, 4, 26) - datetime.datetime.now()
     response = "Countdown to RussCon 2019: " + str(delta) + "\r\n"
