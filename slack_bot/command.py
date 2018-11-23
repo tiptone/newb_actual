df883d38824788d9b9b498989ee3413f00760667
FrankTheTank <franklin@bitsmasher.net>
2018-11-23 03:45:19 +0000

    add tweet command

--- slack_bot/command.py
+++ slack_bot/command.py
@@ -1,5 +1,10 @@
 import datetime
 from weather import Weather, Unit
+import tweepy
+# Import our credentials from credentials.py
+import sys
+sys.path.append('../')
+from credentials import *
 
 class Command(object):
   def __init__(self):
@@ -8,6 +13,7 @@ class Command(object):
       "cookbook" : self.cookbook,
       "github" : self.github,
       "music" : self.music,
+      "tweet" : self.tweet,
       "weather" : self.weather,
       "when" : self.when
     }
@@ -15,7 +21,10 @@ class Command(object):
   def handle_command(self, user, command):
     response = "<@" + user + ">: "
      
-    if command in self.commands:
+    if (command.split(None, 1)[0] ==  'tweet'):
+      self.commands['tweet'](command.split(None, 1)[1])
+      response += "Sending tweet: " + command.split(None, 1)[1]
+    elif command in self.commands:
       response += self.commands[command]()
     else:
       response += "Sorry I don't understand the command: " + command + ". " + self.help()
@@ -28,6 +37,13 @@ class Command(object):
     return "https://github.com/theDevilsVoice/newb_actual/tree/master/slack_bot"
   def music(self):
     return "https://www.reverbnation.com/angusmohr/song/14449187-andy-renwicks-favourite-ferret"
+  def tweet(self, my_tweet):
+    global api
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    api.update_status(my_tweet)
+    return ('Tweet sent.')
   def weather(self):
     weather = Weather(unit=Unit.FAHRENHEIT)
     lookup = weather.lookup(2400352)
